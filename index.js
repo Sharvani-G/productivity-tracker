fbee880d7ca579aa1ff168d792dabcba1ac0578d
Sharvani G <sharvanigbhaskar@gmail.com>
2025-10-08 15:28:31 +0000

    Commit all changes

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 import express from "express";
-import bodyParser from "body-parser";
 import path from 'path';
 import { fileURLToPath } from 'url'; 
 
 const app = express();
 const PORT = 3000; 
+
+
 const __filename = fileURLToPath(import.meta.url);
+
 const __dirname = path.dirname(__filename);
-app.set('views', path.join(__dirname, 'views')); 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get('/', (req, res) => {
-  res.render('partials.ejs');  // Renders index.ejs
+  res.render('index');  
 });
 
 
